5c0e89da0694520448b83208874cf8b967d7d29e
esraa.sayedelsayed <esraa.sayedelsayed@vodafone.com>
2026-08-13 03:04:01 +0300

    finish search component and card

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { View } from 'react-native';
+import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
+import Search from '../../components/Search/Search';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import Tab from '../../components/Tab/Tab';
 import Badge from '../../components/Badge/Badge';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { horizontalScale } from '../../assets/styles/scaling';
+
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
@@ -19,6 +26,36 @@ const Home = () => {
       <Tab title={'Highlight'} />
       <Tab title={'Highlight'} isInactive={true} />
       <Badge title={'Environment'} />
+      <FontAwesomeIcon icon={faSearch} />
+      <Search
+        onSearch={value => {
+          console.log(value);
+        }}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: horizontalScale(24),
+        }}
+      >
+        <SingleDonationItem
+          uri={
+            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
+          }
+          badgeTitle={'Environment'}
+          donationTitle={'Tree Cactus'}
+          price={44}
+        />
+        <SingleDonationItem
+          uri={
+            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
+          }
+          badgeTitle={'Environment'}
+          donationTitle={'Tree Cactus'}
+          price={44}
+        />
+      </View>
     </SafeAreaView>
   );
 };
